8f9008a738943ea386773b211ed46a1d4d302623
Jack Whisler <Jack.Whisler@keypathedu.com>
2022-12-19 11:45:49 -0600

    imports scss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "bootstrap";
 import "react-bootstrap";
 import {render} from "react-dom"
-import "./App.css";
+import "./App.scss";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Home from "./features/Home.tsx";
