6871f25eadbde2cf28807e81d9602728bd693dc9
Jack Whisler <Jack.Whisler@keypathedu.com>
2022-12-20 11:41:06 -0600

    preps for problems

--- src/features/Home.tsx
+++ src/features/Home.tsx
@@ -1,32 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Col } from "react-bootstrap";
 import Row from "react-bootstrap/esm/Row";
 
 function Home() {
-  return (
-    <div className="tile">
-      <Row className="tile">
-        <Col className="bg-dark tile">a</Col>
-        <Col className="bg-primary tile"></Col>
-        <Col className="bg-dark tile"></Col>
-        <Col className="bg-primary"></Col>
-        <Col className="bg-dark"></Col>
-        <Col className="bg-primary"></Col>
-        <Col className="bg-dark"></Col>
-        <Col className="bg-primary"></Col>
-        <Col className="bg-dark"></Col>
-        <Col className="bg-primary"></Col>
-        <Col className="bg-dark"></Col>
-        <Col className="bg-primary"></Col>
-        <Col className="bg-dark"></Col>
-        <Col className="bg-primary"></Col>
-        <Col className="bg-dark"></Col>
-        <Col className="bg-primary"></Col>
-        <Col className="bg-dark"></Col>
-        <Col className="bg-primary"></Col>
+  const caloriesInput: Array<number | string> = [];
+  const [input, setInput] = useState<string>("");
 
-      </Row>
-    </div>
+  const calorieCounting = () => {
+    const inputArr = input.split(/\n/);
+    for (let i = 0; i < inputArr.length; i++) {
+      caloriesInput.push(inputArr[i]);
+    }
+    console.log(caloriesInput);
+  };
+
+  const handleInput = (e) => {
+    setInput(e.target.value);
+    calorieCounting();
+  };
+
+  return (
+    <>
+      <div className="tile">
+        <form>
+          <label>
+            Input:
+            <textarea value={input} onChange={handleInput} />
+          </label>
+        </form>
+
+        <Row className="tile">
+          <Col className="bg-dark tile">a</Col>
+          <Col className="bg-primary tile"></Col>
+          <Col className="bg-dark tile"></Col>
+          <Col className="bg-primary"></Col>
+          <Col className="bg-dark"></Col>
+          <Col className="bg-primary"></Col>
+          <Col className="bg-dark"></Col>
+          <Col className="bg-primary"></Col>
+          <Col className="bg-dark"></Col>
+          <Col className="bg-primary"></Col>
+          <Col className="bg-dark"></Col>
+          <Col className="bg-primary"></Col>
+          <Col className="bg-dark"></Col>
+          <Col className="bg-primary"></Col>
+          <Col className="bg-dark"></Col>
+          <Col className="bg-primary"></Col>
+          <Col className="bg-dark"></Col>
+          <Col className="bg-primary"></Col>
+        </Row>
+      </div>
+    </>
   );
 }
 
